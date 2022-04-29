c5907ca340f7c3aece1ccc5bc133bb7167bb43f5
JollypopChannel <75005411+JollypopChannel@users.noreply.github.com>
2022-04-29 22:16:41 +0700

    v1.0
    The first version of the website so far.
    All the js code was written in Visual Studio Code.
    It's copied and pasted here.
    
    I haven't add style.css in yet bc I wanna see what the site looks like without any make-up of Cascading Style Sheets (CSS).

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,47 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+<!DOCTYPE html>
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+<html>
+    <head>
+        <title>First nontGcob.netlify.app</title>
+        <link href="style.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <meta name="viewport" content="width=device-width, minimum-scale=1.0, initial-scale=1.0">
+        <!-- FUNCTION -->
+        <script>
+            // CLOSE POP-UP
+            function myFunction() {
+                const element = document.getElementById("pop-up");
+                element.remove();
+            }
+        </script>
+    </head>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+    <body class="blackground">
+        <!-- HEADER -->
+        <div class="header">
+            <h1 class="font">- Welcome to nontGcob! -</h1>
+            <h4 class="font2">This is my very first website. I'm so happy to have you as a part of my programming journey.</h4>
+        </div>
 
-      <Footer />
-    </div>
-  )
-}
+        <!-- BORDER -->
+        <div class="adjust-border left2 border border-size">
+            <!-- USERNAME -->
+            <div class="center top3 form-floating mb-3">
+                <input type="username" class="adjust-size form-control" id="floatingUsername" placeholder="Username">
+                <label for="floatingUsername">Username</label>
+            </div>
+
+            <!-- PASSWORD -->
+            <div class="center top4 bottom3 form-floating">
+                <input type="password" class="adjust-size form-control" id="floatingPassword" placeholder="Password">
+                <label for="floatingPassword">Password</label>
+            </div>
+        </div>
+
+        <!-- POP-UP WITH 'x' TO CLOSE THE POP-UP -->
+        <div id="pop-up" class="bottom4 left size alert alert-primary" role="alert">
+            <button id="pop-up" onclick="myFunction()" type="button" class="top2 right2 btn-close" aria-label="one"></button>
+            <p id="pop-up" class="top">Currently, this website is in the developing stage.</p>
+        </div>
+    </body>
+</html>
